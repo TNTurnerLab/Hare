2b787e93eb72dd1fafbf859fa84b8604b52d0aa9
jng2 <jng2@luc.edu>
2022-10-23 16:12:57 -0500

    [CODE] CHROM_LENGTH Update
    Made chromosome length function work also removed unnecessary print statements/commented out lines

--- test_intersect.py
+++ test_intersect.py
@@ -5,9 +5,8 @@ import tabix
 def read_file(file):
     tb=tabix.open(file)
     vcf=[]
-    #print(chromosome_lengths)
     for x,y in chromosome_lengths.items():
-        record=tb.query("chr"+str(x),1,y)  
+        record=tb.query(str(x),1,y)  
         vcf.append(list(record))
     return vcf
 def findVariant(variants):
@@ -31,39 +30,38 @@ def find_intersection(found_variants,name):
     hc_var=found_variants[0][0]
     dv_var=found_variants[1][0]
     dv_dict=found_variants[1][1]
-    #print(dv_dict)
-    combined=list(hc_var.intersection(dv_var))
-    #print(len(combined))
     combined=list(dv_var.intersection(hc_var))
-    #print(len(combined))
+
     with open(name+'_combined_out.vcf','w') as out:
         for f in combined:
             out.write("\t".join(dv_dict[f])+'\n')
 
-
 parser = ArgumentParser(description='Grab info for filter')
-parser.add_argument('-f','--chromosome_lengths',help='Optional tab delimited chromosome file, if not using GRCh38',default=False)
+parser.add_argument('-c','--chromosome_lengths',help='Optional tab delimited chromosome file, if not using GRCh38')
 parser.add_argument('-g','--gatk',help='Path to gatk output')
 parser.add_argument('-d','--deepvariant',help='Path to DeepVariant output')
+parser.add_argument('-r','--ref',help='First line of reference file to see if chr is in fasta')
+
 args = parser.parse_args()
 chromosome_lengths={}
-if args.chromosome_lengths ==False:
+chrom=0
+if 'chr' in args.ref:
+    chrom=1
+if args.chromosome_lengths =='None' and chrom==0:
     chromosome_lengths={1:248956422,2:242193529,3:198295559,4:190214555,5:181538259,6:170805979,7:159345973,8:145138636,9:138394717,10:133797422,11:135086622,12:133275309,13:114364328,14:107043718,15:101991189,16:90338345,17:83257441,18:80373285,19:58617616,20:64444167,21:46709983,22:50818468,'X':156040895}
+elif args.chromosome_lengths =='None' and chrom==1:
+    chromosome_lengths={'chr1':248956422,'chr2':242193529,'chr3':198295559,'chr4':190214555,'chr5':181538259,'chr6':170805979,'chr7':159345973,'chr8':145138636,'chr9':138394717,'chr10':133797422,'chr11':135086622,'chr12':133275309,'chr13':114364328,'chr14':107043718,'chr15':101991189,'chr16':90338345,'chr17':83257441,'chr18':80373285,'chr19':58617616,'chr20':64444167,'chr21':46709983,'chr22':50818468,'chrX':156040895}
+
 else:
+
     with open(args.chromosome_lengths) as input:
         for line in input:
             data=line.strip().split('\t')
-            if data[0].startswith('chr'):
-
-                chromosome_lengths[int(data[0].split('chr')[1])]=int(data[1])
-            else:
-                chromosome_lengths[int(data[0])]=int(data[1])
+            chromosome_lengths[data[0]]=int(data[1])
 name=args.gatk.split('.glnexus_denovo_actual.hc.vcf.gz')[0]
-print(name)    
 input=[args.gatk,args.deepvariant]
 read_files=process_files_parallel(input)
 
 found_variants=findVariant_parallel(read_files)
 find_intersection(found_variants,name)
 
-#print(len(hello))
